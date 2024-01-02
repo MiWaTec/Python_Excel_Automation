a484151826d52e8b457ce620dd8bfcca1d67b600
Miwa <warsitzm@gmail.com>
2024-01-02 22:16:12 +0100

    Last changes

--- InsertColumn/insert_column_excel.py
+++ InsertColumn/insert_column_excel.py
@@ -5,7 +5,7 @@ file_path = "C:/Users/Michael/Desktop/ExcelData\Client_Data.xlsx"
 df = pd.read_excel(file_path)
 
 # Insert an empty column 
-col_index =df.columns.get_loc('Weight (kg)')
+col_index = df.columns.get_loc('Weight (kg)')
 df.insert(col_index+1, 'BMI2', None)
 df['BMI'] = df['Weight (kg)'] / (df['Height (cm)']/100)**2
 
