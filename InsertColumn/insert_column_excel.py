df419d427e395017672227ae0b90e765eb8283cc
Miwa <warsitzm@gmail.com>
2024-01-03 00:27:14 +0100

    Example adjusted

--- InsertColumn/insert_column_excel.py
+++ InsertColumn/insert_column_excel.py
@@ -1,13 +1,13 @@
 import pandas as pd
 
 # Get dataframe from excel file
-file_path = "C:/Users/Michael/Desktop/ExcelData\Client_Data.xlsx"
+file_path = "C:/Users/warsi/Desktop/ExcelData\Client_Data.xlsx"
 df = pd.read_excel(file_path)
 
 # Insert an empty column 
 col_index = df.columns.get_loc('Weight (kg)')
-df.insert(col_index+1, 'BMI2', None)
-df['BMI'] = df['Weight (kg)'] / (df['Height (cm)']/100)**2
+df.insert(col_index+1, 'BMI', None)
+df['BMI'] = round(df['Weight (kg)'] / (df['Height (cm)']/100)**2, 1)
 
 # Update excel file with new column
 df.to_excel(file_path, index=False)
