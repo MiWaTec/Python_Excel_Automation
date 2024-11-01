6d4cb6553b9d2ae2b7c9eac4241c3729343f9f8e
Miwa <warsitzm@gmail.com>
2024-11-02 00:20:29 +0100

    functions.py extended gui_main.py extended

--- Combine_Excel_Files/functions.py
+++ Combine_Excel_Files/functions.py
@@ -3,6 +3,7 @@ import pandas as pd
 import tkinter as tk
 from tkinter import filedialog
 import customtkinter as ctk
+from PIL import Image
 
 def browse_folder(input_line):
     """This function will open a window from which the folder can be chosen.
@@ -32,31 +33,103 @@ def getExcelFiles(folder_path) -> list:
     return excel_list
 
 
-def displayExcelFiles(scrollable_frame, excel_list):
+def displayExcelList(scrollable_frame, select_all, excel_list, switch_state):
+    # Load label icon
+    icon = Image.open('Images/Excel_Icon_2.png')
+    label_icon = ctk.CTkImage(icon)
+    # Clear scrollable frame
+    for widget in scrollable_frame.winfo_children():
+        widget.destroy()
+    # Reset 'Select all' switch to 0
+    if select_all.get() == 1:
+        select_all.deselect()
+    # Check display mode
+    if switch_state == 0:
+        displayExcelFiles(scrollable_frame, excel_list, label_icon)
+    elif switch_state == 1:
+        displayExcelWorksheets(scrollable_frame, excel_list, label_icon)
+
+
+def displayExcelWorksheets(scrollable_frame, excel_list, label_icon):
+    row_counter = 0
+    for item in excel_list:
+        label = ctk.CTkLabel(scrollable_frame,
+                             text=item[1],
+                             image=label_icon,
+                             compound='left',
+                             padx=5)
+        label.grid(row=row_counter, column=0, padx=5, sticky='w')
+        row_counter += 1
+        # Get all worksheets
+        excel_file = pd.ExcelFile(item[0])
+        sheet_names = excel_file.sheet_names
+        # Display worksheets
+        for worksheet in sheet_names:
+            checkbox_ws = ctk.CTkCheckBox(scrollable_frame,
+                                          checkbox_width=20,
+                                          checkbox_height=20,
+                                          fg_color='#21A366',
+                                          hover_color='#66E383',
+                                          text=worksheet)
+            checkbox_ws.grid(row=row_counter, column=0, padx=35, pady=[0, 0], sticky='w')
+            row_counter += 1
+
+
+def displayExcelFiles(scrollable_frame, excel_list, label_icon):
     for index, item in enumerate(excel_list):
-        checkbox = ctk.CTkCheckBox(scrollable_frame, text=item[1])
-        checkbox.grid(row=index, column=0, padx=5, pady=[5, 0], sticky='w')
+        checkbox = ctk.CTkCheckBox(scrollable_frame,
+                                   checkbox_width=25,
+                                   checkbox_height=25,
+                                   fg_color='#21A366',
+                                   hover_color='#66E383',
+                                   text='')
+        checkbox.grid(row=index, column=0, padx=5, pady=5, sticky='w')
+        label = ctk.CTkLabel(scrollable_frame,
+                             text=item[1],
+                             image=label_icon,
+                             compound='left',
+                             padx=5)
+        label.grid(row=index, column=0, padx=35, sticky='w')
 
 
-def combine_excel_files(available_files, input_path, output_path, output_name):
+def toggle_all_checkboxes(switch_state, widgets):
+    for widget in widgets:
+        if isinstance(widget, ctk.CTkCheckBox):
+            if switch_state == 0:
+                widget.deselect()
+            elif switch_state == 1:
+                widget.select()
+
+
+def combineExcelFiles(available_files, input_path, output_path, output_name):
     # Create excel dict with file names as key and paths as values
     excel_file_paths = getExcelFiles(input_path)
     excel_dict = {}
     for excel in excel_file_paths:
         excel_dict[excel[1]] = excel[0]
-    # Save the paths of the selected excel files into a list
-    selected_excel_list = []
-    for checkbox in available_files.winfo_children():
-        if checkbox.get() == 1:
-            path = excel_dict[checkbox.cget('text')]
-            selected_excel_list.append(path)
+    # Save the paths of the selected excel files into a dict
+    selected_excel_dict = {}
+    current_excel_file = None
+    current_sheetnames = None
+    for widget in available_files.winfo_children():
+        if isinstance(widget, ctk.CTkLabel):
+            if widget.cget('text') in excel_dict:
+                current_excel_file = widget.cget('text')
+                excel_file = pd.ExcelFile(excel_dict[widget.cget('text')])
+                current_sheetnames = excel_file.sheet_names
+                selected_excel_dict[excel_dict[current_excel_file]] = []
+        if isinstance(widget, ctk.CTkCheckBox):
+            # for sheet in current_sheetnames:
+            if widget.get() == 1:
+                selected_excel_dict[excel_dict[current_excel_file]].append(widget.cget('text'))
     # Merge all selected excel files to a single dataframe
     df_merged = None
-    for excel_file in selected_excel_list:
-        df = pd.read_excel(excel_file)
-        df_merged = pd.concat([df_merged, df], ignore_index=True, sort=False)
+    for item in selected_excel_dict:
+        excel_file = pd.ExcelFile(item)
+        for ws in selected_excel_dict[item]:
+            df = excel_file.parse(ws)
+            df_merged = pd.concat([df_merged, df], ignore_index=True, sort=False)
     # Write dataframe into a new excel file
     merged_excel = os.path.join(output_path, output_name + '.xlsx')
-    print(merged_excel)
     df_merged.to_excel(merged_excel)
     return df_merged
--- Combine_Excel_Files/gui_main.py
+++ Combine_Excel_Files/gui_main.py
@@ -1,82 +1,135 @@
 import customtkinter as ctk
+from PIL import Image, ImageTk
 import functions as func
+import ctypes
 
+ctypes.windll.shcore.SetProcessDpiAwareness(1)
 
 # Create window
 window = ctk.CTk()
-window.geometry('500x480')
-window.columnconfigure(0, weight=1)
-window.title('Combine Excel Files')
+window.geometry('750x605')
+window.resizable(False, False)
+window.columnconfigure(0, weight=0)
+window.columnconfigure(1, weight=2)
+window.title('Excel File Merger')
+
+# Add image
+src_img = Image.open('Images/Excel_Design_2.jpg')
+src_img = src_img.resize((200, 740))
+image = ImageTk.PhotoImage(src_img)
+
+img_frame = ctk.CTkFrame(window)
+img_frame.grid(row=0, column=0, rowspan=3, padx=(5, 2), pady=(5, 5))
+img_frame.columnconfigure(1, weight=1)
+
+img_label = ctk.CTkLabel(img_frame,
+                         image=image)
+img_label.grid(row=0, column=0)
 
 # Set color
-frame_color = '#300666'
+frame_color = '#1F9B69'
 
 # Select folder and search for files
 frame_select_folder = ctk.CTkFrame(window, fg_color=frame_color)
-frame_select_folder.grid(row=0, column=0, padx=5, pady=5, sticky='we')
-frame_select_folder.columnconfigure(0, weight=1)
+frame_select_folder.grid(row=0, column=1, padx=5, pady=5, sticky='we')
+frame_select_folder.columnconfigure(1, weight=1)
 
 title_label = ctk.CTkLabel(frame_select_folder, text='Select folder:',
                            font=ctk.CTkFont(size=12, weight='bold'))
-title_label.grid(row=0, column=0, padx=[20, 0], sticky='ws')
+title_label.grid(row=0, column=1, padx=[20, 0], sticky='ws')
 
 input_folder = ctk.CTkEntry(frame_select_folder)
-input_folder.grid(row=1, column=0, padx=[20, 0], pady=[0, 15], sticky='we')
+input_folder.grid(row=1, column=1, padx=[20, 0], pady=[0, 15], sticky='we')
 
 btn_browse = ctk.CTkButton(frame_select_folder, text='Browse',
                            width=75,
+                           fg_color='#06361B',
+                           hover_color='#66E383',
                            command=lambda: [func.browse_folder(input_folder),
-                                            func.displayExcelFiles(scrollable_frame, func.getExcelFiles(input_folder.get()))])
-btn_browse.grid(row=1, column=1, padx=[0, 20], pady=[0, 15])
+                                            func.displayExcelList(
+                                                scrollable_frame,
+                                                select_all,
+                                                func.getExcelFiles(input_folder.get()),
+                                                display_mode.get())])
+btn_browse.grid(row=1, column=2, padx=[0, 20], pady=[0, 15])
 
 # Select excel files
-frame_select_files = ctk.CTkFrame(window, fg_color=frame_color)
-frame_select_files.grid(row=1, column=0, padx=5, pady=5, sticky='we')
-frame_select_files.columnconfigure(0, weight=1)
+frame_select_files = ctk.CTkFrame(window,
+                                  fg_color=frame_color)
+frame_select_files.grid(row=1, column=1, padx=5, pady=5, sticky='we')
+frame_select_files.columnconfigure(1, weight=1)
 
 select_files_label = ctk.CTkLabel(frame_select_files,
                                   text='Select Excel files:',
                                   font=ctk.CTkFont(size=12, weight='bold'))
 select_files_label.grid(row=0, column=0, padx=[20, 0], pady=[0, 0], sticky='w')
 
-scrollable_frame = ctk.CTkScrollableFrame(frame_select_files)
-scrollable_frame.grid(row=1, column=0, padx=20, pady=[0, 15], sticky='we')
+scrollable_frame = ctk.CTkScrollableFrame(frame_select_files,
+                                          scrollbar_button_hover_color='#66E383',
+                                          height=295)
+scrollable_frame.grid(row=1, column=0, columnspan=2, padx=20, pady=[0, 15], sticky='we')
+
+select_all = ctk.CTkSwitch(frame_select_files,
+                           text='Select all',
+                           font=ctk.CTkFont(size=12, weight='bold'),
+                           button_color='#06361B',
+                           progress_color='#66E383',
+                           hover=False,
+                           command=lambda: func.toggle_all_checkboxes(select_all.get(),
+                                                                      scrollable_frame.winfo_children()))
+select_all.grid(row=2, column=0, padx=20, pady=[0, 15], sticky='w')
+
+display_mode = ctk.CTkSwitch(frame_select_files,
+                           text='Display worksheets',
+                           font=ctk.CTkFont(size=12, weight='bold'),
+                           button_color='#06361B',
+                           progress_color='#66E383',
+                           hover=False,
+                           command=lambda: func.displayExcelList(scrollable_frame,
+                                                                 select_all,
+                                                                 func.getExcelFiles(input_folder.get()),
+                                                                 display_mode.get()))
+display_mode.grid(row=2, column=1, padx=20, pady=[0, 15], sticky='w')
 
 # Combine excel files
 frame_combine_files = ctk.CTkFrame(window, fg_color=frame_color)
-frame_combine_files.grid(row=2, column=0, padx=5, pady=5, sticky='we')
-frame_combine_files.columnconfigure(0, weight=1)
+frame_combine_files.grid(row=2, column=1, padx=5, pady=5, sticky='we')
+frame_combine_files.columnconfigure(1, weight=1)
 
 save_in_folder_label = ctk.CTkLabel(frame_combine_files,
                                     text='Save merged Excel file in:',
                                     font=ctk.CTkFont(size=12, weight='bold'))
-save_in_folder_label.grid(row=0, column=0, padx=[20, 0], sticky='ws')
+save_in_folder_label.grid(row=0, column=1, padx=[20, 0], sticky='ws')
 
 input_save_merged_excel = ctk.CTkEntry(frame_combine_files)
-input_save_merged_excel.grid(row=1, column=0, padx=[20, 0], sticky='we')
+input_save_merged_excel.grid(row=1, column=1, padx=[20, 0], sticky='we')
 
 btn_browse_save_folder = ctk.CTkButton(frame_combine_files, text='Browse',
                                        width=75,
+                                       fg_color='#06361B',
+                                       hover_color='#66E383',
                                        command=lambda:
                                        func.browse_folder(input_save_merged_excel))
-btn_browse_save_folder.grid(row=1, column=1, padx=[0, 20], sticky='we')
+btn_browse_save_folder.grid(row=1, column=2, padx=[0, 20], sticky='we')
 
 save_in_folder_label = ctk.CTkLabel(frame_combine_files,
                                     text='File name:',
                                     font=ctk.CTkFont(size=12, weight='bold'))
-save_in_folder_label.grid(row=2, column=0, padx=[20, 0], sticky='ws')
+save_in_folder_label.grid(row=2, column=1, padx=[20, 0], sticky='ws')
 
 input_save_combined_excel = ctk.CTkEntry(frame_combine_files)
-input_save_combined_excel.grid(row=3, column=0, padx=[20, 0], pady=[0, 15],
+input_save_combined_excel.grid(row=3, column=1, padx=[20, 0], pady=[0, 15],
                                sticky='we')
 
 btn_search_files = ctk.CTkButton(frame_combine_files,
                                  width=75, text='Merge',
-                                 command=lambda: func.combine_excel_files(scrollable_frame,
-                                                                     input_folder.get(),
-                                                                     input_save_merged_excel.get(),
-                                                                     input_save_combined_excel.get()))
-btn_search_files.grid(row=3, column=1, padx=[0, 20], pady=[0, 15])
+                                 fg_color='#06361B',
+                                 hover_color='#66E383',
+                                 command=lambda: func.combineExcelFiles(scrollable_frame,
+                                                                        input_folder.get(),
+                                                                        input_save_merged_excel.get(),
+                                                                        input_save_combined_excel.get()))
+btn_search_files.grid(row=3, column=2, padx=[0, 20], pady=[0, 15])
 
 # Wait for interaction with the GUI
 window.mainloop()
